0e957e2031226d477fb18a0794e2a9adef1c6a51
Júnior Silva <junior-vocal@hotmail.com>
2025-03-26 10:17:20 -0300

    26/03/2025

--- javascript.js
+++ javascript.js
@@ -6,3 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileMenu.classList.toggle("active");
     });
 });
+
+// ENVIAR MENSAGEM //
+
+function enviarWhatsApp() {
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+    const local = document.getElementById('local').value;
+    const data = document.getElementById('data').value;
+    const detalhes = document.getElementById('detalhes').value;
+
+    const numeroDestino = "5535984368959";
+    
+    const mensagem = `Olá, meu nome é *${nome}*. Gostaria de agendar um show.
+    📍 Local: *${local}*  
+    📅 Data: *${data}*  
+    📞 Telefone: *${telefone}*  
+    📝 Detalhes: *${detalhes}*`;
+
+    const url = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(mensagem)}`;
+    window.open(url, '_blank');
+}
+
